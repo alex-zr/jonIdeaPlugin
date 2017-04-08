6bdfa9c52e21f2f01027196c47254b784d08103d
salvador_sakho <Salvador.Sakho21@gmail.com>
2017-04-08 18:05:07 +0300

    Fixed structure creation methods.

--- ModuleAfterAlexandrCorrections/src/actions/TaskDownloader.java
+++ ModuleAfterAlexandrCorrections/src/actions/TaskDownloader.java
@@ -4,13 +4,10 @@ import service.SwingTaskTable;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-import java.util.logging.*;
-
 /**
  * Created by ${BIM} on 11.11.2016.
  */
 public class TaskDownloader extends AnAction {
-    private static Logger log = Logger.getLogger(TaskDownloader.class.getName());
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
